4000beb13d0f5e49ebe6770ad07819d00662df83
XuYouye <15298560573@163.com>
2020-08-11 11:45:20 +0800

    Jenkins集成测试

--- mytest/src/main/java/XuDTest02/Student.java
+++ mytest/src/main/java/XuDTest02/Student.java
@@ -4,9 +4,17 @@ package XuDTest02;
 public class Student {
     public int id;
     public String name;
+    public String phonenum;
 
     @Override
     public String toString() {
-        return "Student [id=" + id + ", name=" + name + "]";
+        return "Student [id=" + id + ", name=" + name + ",phonenum= "+ phonenum +"]";
+       
     }
+    public static  void main(String args[]) {
+    	System.out.println("Jenkins集成测试");
+    	System.out.println("JenkinsTest集成测试");
+    }
+    
+    
 }
